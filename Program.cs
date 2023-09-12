cac1a41f3d0d0b29d75ba0d0312d9aea4073a905
Nithya Subramaniyan <nithya.subramaniyan@itp.objectfrontier.com>
2023-09-12 15:27:09 +0530

    branch n added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿Console.WriteLine("Hello, Laevi!");
-Console.WriteLine("What are you doing");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Laevi!");
+        Console.WriteLine("What are you doing");
+        Console.Write("Nithya is here why fear....");
+    }
+}
